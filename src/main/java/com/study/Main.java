f1a5f6ce4a4b1045eca9bcf5b9ff055d1f57e4cb
xiaodong <531849939@qq.com>
2019-06-25 11:31:24 +0800

    m

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -1,120 +1,12 @@
 package com.study;
 
-import com.study.tree.BinaryTreeNode;
-import com.study.tree.BinaryTreeReadMain;
+import org.apache.commons.lang.time.FastDateFormat;
 
-import java.util.Stack;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        BinaryTreeNode node = BinaryTreeReadMain.prepare();
-        first(node);
-        System.out.println();
-        first1(node);
-        System.out.println();
-        System.out.println();
 
-        mid(node);
-        System.out.println();
-        mid1(node);
-        System.out.println();
-        System.out.println();
-
-        after(node);
-        System.out.println();
-        after1(node);
-        System.out.println();
-        System.out.println();
     }
-
-    private static void after1(BinaryTreeNode node) {
-        Stack<BinaryTreeNode> stack = new Stack<>();
-        while (true) {
-            if (null != node) {
-                stack.push(node);
-                node = node.getLeft();
-            } else {
-                if (stack.isEmpty()) {
-                    break;
-                }
-                if (null == stack.peek().getRight()) {
-                    node = stack.pop();
-                    System.out.print(node.getValue() + "\t");
-                    while (node == stack.peek().getRight()) {
-                        node = stack.pop();
-                        System.out.print(node.getValue() + "\t");
-                        if (stack.isEmpty()) {
-                            break;
-                        }
-                    }
-                }
-                if (!stack.isEmpty()) {
-                    node = stack.peek().getRight();
-                } else {
-                    node = null;
-                }
-            }
-        }
-    }
-
-    private static void after(BinaryTreeNode node) {
-        if (null == node) {
-            return;
-        }
-        after(node.getLeft());
-        after(node.getRight());
-        System.out.print(node.getValue() + "\t");
-    }
-
-    private static void mid1(BinaryTreeNode node) {
-        Stack<BinaryTreeNode> stack = new Stack<>();
-        while (true) {
-            while (null != node) {
-                stack.push(node);
-                node = node.getLeft();
-            }
-            if (stack.isEmpty()) {
-                break;
-            }
-            node = stack.pop();
-            System.out.print(node.getValue() + "\t");
-            node = node.getRight();
-        }
-    }
-
-    private static void mid(BinaryTreeNode node) {
-        if (null == node) {
-            return;
-        }
-        mid(node.getLeft());
-        System.out.print(node.getValue() + "\t");
-        mid(node.getRight());
-    }
-
-    private static void first1(BinaryTreeNode node) {
-        Stack<BinaryTreeNode> stack = new Stack<>();
-        while (true) {
-            while (node != null) {
-                stack.push(node);
-                System.out.print(node.getValue() + "\t");
-                node = node.getLeft();
-            }
-            if (stack.isEmpty()) {
-                break;
-            }
-            node = stack.pop();
-            node = node.getRight();
-        }
-    }
-
-    private static void first(BinaryTreeNode node) {
-        if (null == node) {
-            return;
-        }
-        System.out.print(node.getValue() + "\t");
-        first(node.getLeft());
-        first(node.getRight());
-    }
-
 }
--- src/main/scala/com/study/flink/EventTimeWindow.scala
+++ src/main/scala/com/study/flink/EventTimeWindow.scala
@@ -1,5 +1,6 @@
 package com.study.flink
 
+import org.apache.commons.lang.StringUtils
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
@@ -9,17 +10,34 @@ import org.apache.flink.streaming.api.windowing.time.Time
 object EventTimeWindow {
 
     def main(args: Array[String]): Unit = {
+        val split = " "
+
         val env = StreamExecutionEnvironment.getExecutionEnvironment
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
         env.setParallelism(1)
         val text: DataStream[String] = env.socketTextStream("localhost", 9998, '\n')
-        text.assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[String](Time.seconds(15)) {
+        text.filter(line => StringUtils.isNotEmpty(line) && line.split(split, -1).length > 1)
+            .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[String](Time.seconds(5)) {
             override def extractTimestamp(t: String): Long = {
-                t.split("\t", -1)(0).toLong
+                try {
+                    val mills = t.split(split, -1)(0).toLong
+                    mills
+                } catch {
+                    case _: Exception => 0
+                }
             }
-        } ).keyBy(_.split("\t")(1))
+        }).map(line => {(getFloorTime(line.split(split, -1)(0).toLong, 6) + line.split(split, -1)(1), 1)})
+            .keyBy(_._1)
             .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+            .sum(1)
+            .setParallelism(1)
+            .print()
 
+        env.execute("EventTimeWindow")
+    }
+
+    private def getFloorTime(mills: Long, interval: Int): Long = {
+        mills / (interval * 1000) * interval * 1000
     }
 
 }
